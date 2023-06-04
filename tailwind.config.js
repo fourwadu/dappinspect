49066371a2822c0dd00fed8f3c219e5c6348a3cd
noah <30538387+fourwadu@users.noreply.github.com>
2023-06-04 00:32:59 -0400

    rename

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{tsx,ts}", "./public/index.html", "./public/**/*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-dark": "#15141A",
+				"primary-light": "1E1C2C",
+				"primary-gray": "#808080",
+				"primary-red": "EC4A4A",
+				"secondary-light": "#2F2C3A",
+			},
+		},
 	},
 	plugins: [],
 };
